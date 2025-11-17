8258ed3f10bde37fb397305f634e2090c6515c0a
Lafferton, Jan <jan.lafferton@vector.com>
2025-11-17 13:19:10 +0100

    Add push constants to shader

--- raytracer-gpu/src/lib.rs
+++ raytracer-gpu/src/lib.rs
@@ -1,11 +1,18 @@
 #![expect(unexpected_cfgs)]
 #![cfg_attr(target_arch = "spirv", no_std)]
 
-use spirv_std::glam::{UVec2, UVec3, Vec3Swizzles, vec4};
+use spirv_std::glam::{Mat4, UVec2, UVec3, Vec3Swizzles, vec4};
 use spirv_std::{Image, spirv};
+
+pub struct PushConstants {
+    num_accumulated_frames: usize,
+    inverse_projection: Mat4,
+    inverse_view: Mat4,
+}
 
 #[spirv(compute(threads(10, 10)))]
 pub fn main_cs(
+    #[spirv(push_constant)] push_constants: &PushConstants,
     #[spirv(global_invocation_id)] global_invocation_id: UVec3,
     #[spirv(descriptor_set = 0, binding = 0)] output: &Image!(2D, format = rgba8, sampled = false),
 ) {
