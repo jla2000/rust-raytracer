ca14cb7b416aed4e4af94c9f82b9e45d183f6692
Lafferton, Jan <jan.lafferton@vector.com>
2025-11-25 15:43:20 +0100

    Draw uv

--- raytracer-gpu/src/lib.rs
+++ raytracer-gpu/src/lib.rs
@@ -1,17 +1,24 @@
 #![expect(unexpected_cfgs)]
 #![cfg_attr(target_arch = "spirv", no_std)]
 
-use spirv_std::glam::{UVec2, UVec3, Vec3Swizzles, vec4};
+use spirv_std::glam::{UVec3, Vec3Swizzles, Vec4};
 use spirv_std::{Image, spirv};
 
 #[spirv(ray_generation)]
 pub fn generate_rays(
     #[spirv(launch_id)] launch_id: UVec3,
+    #[spirv(launch_size)] launch_size: UVec3,
     #[spirv(descriptor_set = 0, binding = 0)] output: &Image!(2D, format = rgba8, sampled = false),
 ) {
-    let output_size: UVec2 = output.query_size();
-
-    if launch_id.x < output_size.x && launch_id.y < output_size.y {
-        unsafe { output.write(launch_id.xy(), vec4(1.0, 0.0, 1.0, 1.0)) };
-    }
+    unsafe {
+        output.write(
+            launch_id.xy(),
+            Vec4::new(
+                launch_id.x as f32 / launch_size.x as f32,
+                launch_id.y as f32 / launch_size.y as f32,
+                0.0,
+                1.0,
+            ),
+        )
+    };
 }
--- raytracer/build.rs
+++ raytracer/build.rs
@@ -3,11 +3,10 @@ use spirv_builder::{Capability, MetadataPrintout, SpirvBuilder};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     SpirvBuilder::new("../raytracer-gpu", "spirv-unknown-vulkan1.4")
         .print_metadata(MetadataPrintout::Full)
-        .extension("SPV_KHR_ray_query")
         .extension("SPV_KHR_ray_tracing")
-        .capability(Capability::ImageQuery)
-        // .capability(Capability::RayQueryKHR)
         .capability(Capability::RayTracingKHR)
+        // .extension("SPV_KHR_ray_query")
+        // .capability(Capability::RayQueryKHR)
         .build()?;
     Ok(())
 }
